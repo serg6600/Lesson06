71b12594bccce7af306cfe58cee247c5deab8e07
Сергей Рожко <serg6600@yandex.ru>
2021-04-13 00:39:16 +0300

    first

--- script.js
+++ script.js
@@ -6,25 +6,44 @@ let isNumber = function(n){
 
 alert('Загадывание случайного числа от 1 до 100');
 
-function checking(){
-    const x = 93;
-    let number = prompt('Угадай число от 1 до 100');
-    if (number === null){
-        alert('Игра окончена');
-    } else if (!isNumber(number)) {
-        alert('Введи число!');
-        checking();
-    } else if (x > number){
-        alert('Загаданное число больше');
-        checking();
-    } else if (x < number) {
-        alert('Загаданное число меньше');
-        checking();
-    } else if (x === Number(number)){
-        alert('Поздравляю, Вы угадали!!!');
+function checking(counter){
+    const x = 88;
+    console.log('counter: ', counter);
+    //Проверка после окончания попыток
+    if (counter === 0){
+        if (confirm('Попытки закончились, хотите сыграть еще?')){
+            counter = 10;
+            checking(counter);
+        } else {
+            alert('Благодарим вас за игру');
+            return;
+        }
     }
+    //Сама игра
+        let number = prompt('Угадай число от 1 до 100');
+        if (number === null){
+            alert('Игра окончена');
+        } else if (!isNumber(number)) {
+            alert('Введи число!');
+            checking();
+        } else if (x > number){
+            counter--;
+            alert('Загаданное число больше, осталось попыток: ' + counter);
+            checking(counter);
+        } else if (x < number) {
+            counter--;
+            alert('Загаданное число меньше, осталось попыток: ' + counter);
+            checking(counter);
+        } else if (x === Number(number)){ //При угадывании выход или еще играем
+            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')){
+                counter = 10;
+                checking(counter);
+            } else {
+                alert('Благодарим вас за игру');
+            }
+        }
 }
 
-checking();
+checking(10);
 
 
